92d77a3807988f1773253cb8d0b82cc188af3c9f
nursakinahroslan <nursakinahroslan@gmail.com>
2016-04-26 17:51:49 +0800

    Initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,47 @@
+<?php
+
+
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * Show Task Dashboard
+ */
+Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('task', [
+        'tasks' => $tasks
+    ]);
+});
+
+/**
+ * Add new Task
+ */
+Route::post('/task', function (Request $request) {
+    $validator =validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * Delete Task
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect('/');
+    //
+});
